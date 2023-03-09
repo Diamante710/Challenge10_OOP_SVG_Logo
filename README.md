ac3430d03c29d4218c38b636327a9b9698a685fa
Diamante710 <diamante710@icloud.com>
2023-03-08 20:29:02 -0700

    ReadMe & Testing  update

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Challenge10_OOP_SVG_Logo
 
+## Table of Contents
+* Task
+* User Story
+* Installation & Usage
+* Testing
+* Screenshot & Demo Link
+
 ## Task
 
-Your task is to build a Node.js command-line application that takes in user input to generate a logo and save it as an SVG file. The application prompts the user to select a color and shape, provide text for the logo, and save the generated SVG to a `.svg` file.
+This application is designed to build a Node.js command-line application that takes in user input to generate a logo and save it as an SVG file. The application prompts the user to select a color and shape, provide text for the logo, and save the generated SVG to a `.svg` file.
 
 ### User Story
 
@@ -11,5 +18,23 @@ AS a freelance web developer
 I WANT to generate a simple logo for my projects
 SO THAT I don't have to pay a graphic designer
 ```
+## Installation & Usage
+
+To install, clone the repository locally in VS Code. Proceed to open an integrated terminal in the root directory and type:
+```md
+npm i
+```
+To use this application, within the integrated terminal type the command below. Once entered the user will be prompted with four questions that will create a logo.svg file upon completion.
+```md
+node index.js
+```
+
+## Testing
+
+To test the application, enter the root directory through an integrated terminal and type:
+```md
+npm test
+```
+Test results will display in the command line.
 
 ## Screenshot & Demo Link
--- main/library/circle.test.js
+++ main/library/circle.test.js
@@ -3,7 +3,7 @@ const {Circle} = require("./shapes");
 
 describe("Circle", () => {
     test("Should render logo.svg file for a black circle element", () => {
-        const expectedSvg = `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg"/><circle cx="25" cy="75" r="20" fill="black"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">test</text></svg>`
+        const expectedSvg = `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg"><circle cx="25" cy="75" r="20" fill="black"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">test</text></svg>`
         const circle = new Circle("circle", "black", "test", "white");
         const actualSvg = circle.render(); 
         expect(actualSvg).toEqual(expectedSvg);
@@ -12,7 +12,7 @@ describe("Circle", () => {
 
 describe("Circle", () => {
     test("Should render logo.svg file for a blue circle element", () => {
-        const expectedSvg = `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg"/><circle cx="25" cy="75" r="20" fill="black"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">test</text></svg>`
+        const expectedSvg = `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg"><circle cx="25" cy="75" r="20" fill="blue"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">test</text></svg>`
         const circle = new Circle("circle", "blue", "test", "white");
         const actualSvg = circle.render(); 
         expect(actualSvg).toEqual(expectedSvg);
